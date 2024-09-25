ce5f4ca67f3c4b6bb7441ba92956bdadda25527f
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-25 23:01:55 +0900

    Title: 특정 숫자 도달하기 2, Time: 134ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240925/특정 숫자 도달하기 2/reaching-specific-number-2.kt
@@ -0,0 +1,12 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    for(i in n downTo 1) {
+        print("$i ")
+    }
+}
