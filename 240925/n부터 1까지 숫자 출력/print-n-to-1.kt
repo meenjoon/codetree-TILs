f6eec18ab4f85178a883f3bb48d0d3dbe8c6839c
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-25 23:25:31 +0900

    Title: n부터 1까지 숫자 출력, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240925/n부터 1까지 숫자 출력/print-n-to-1.kt
@@ -0,0 +1,13 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    var sc = Scanner(System.`in`)
+
+    var n = sc.nextInt()
+
+    while(n >= 1) {
+        print("$n ")
+        n--
+    }
+}
