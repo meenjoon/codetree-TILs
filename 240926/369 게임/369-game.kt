87e576a77cb5f8dd6fba80831900e555590c2f22
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-26 21:36:58 +0900

    Title: 369 게임, Time: 161ms, Memory: 11MB, Status: Passed - CodeTree

--- 240926/369 게임/369-game.kt
+++ 240926/369 게임/369-game.kt
@@ -9,7 +9,7 @@ fun main() {
     for(i in 1..n) {
         val iStr = i.toString()
         val isContains = iStr.contains("3") || iStr.contains("6") || iStr.contains("9")
-        val isConstins2 = (i % 10 == 3) || (i % 10 == 6) || (i % 10 == 9)
+        val isConstins2 = (i % 10 == 3) || (i % 10 == 6) || (i % 10 == 9) || (i / 10 == 3) || (i / 10 == 6) || (i / 10 == 9)
 
         if(i % 3 == 0 || isConstins2) {
             print("0 ")
