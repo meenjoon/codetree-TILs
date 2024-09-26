41e724146d109dbfeb72a32a9ea7713187e923fb
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-26 23:49:55 +0900

    Title: 총합 구하기, Time: 147ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240926/총합 구하기/find-the-total.kt
@@ -0,0 +1,18 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+
+    var sumNum = 0
+    for(i in a..b) {
+        if(i % 6 == 0 && i % 8 != 0){
+            sumNum += i
+        }
+    }
+
+    print("$sumNum")
+}
