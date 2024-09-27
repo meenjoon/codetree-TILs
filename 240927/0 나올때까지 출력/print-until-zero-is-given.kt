4dc75b766824f4516fe41baca531fd1f5e316704
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-27 23:49:40 +0900

    Title: 0 나올때까지 출력, Time: 149ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240927/0 나올때까지 출력/print-until-zero-is-given.kt
@@ -0,0 +1,14 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    while(true) {
+        val n = sc.nextInt()
+        if(n == 0) {
+            break
+        }
+        println("$n")
+    }
+}
