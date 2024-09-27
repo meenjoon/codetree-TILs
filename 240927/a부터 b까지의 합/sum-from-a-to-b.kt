90c8dc6c8dae5865581810be9fe733838f389446
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-27 23:08:23 +0900

    Title: a부터 b까지의 합, Time: 161ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240927/a부터 b까지의 합/sum-from-a-to-b.kt
@@ -0,0 +1,16 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+
+    var sum = 0
+    for(i in 2..5) {
+        sum +=i
+    }
+
+    print("$sum")
+}
