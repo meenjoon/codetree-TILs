8e1409002cd71e8a357bece700568b6abfdfb210
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-28 17:29:13 +0900

    Title: 직각 삼각형 출력, Time: 141ms, Memory: 11MB, Status: Passed - CodeTree

--- 240928/직각 삼각형 출력/Right-triangle-and-output.kt
+++ 240928/직각 삼각형 출력/Right-triangle-and-output.kt
@@ -6,9 +6,19 @@ fun main() {
 
     val n = sc.nextInt()
 
-    var cnt = 1
-    repeat(n) {
-        println("${"*".repeat(cnt)}")
-        cnt += 2
+    // repeat 사용 방법
+    // var cnt = 1
+    // repeat(n) {
+    //     println("${"*".repeat(cnt)}")
+    //     cnt += 2
+    // }
+
+    // for 문 사용 방법
+    for(i in 1..n) {
+        val starNum = (i * 2) - 1 
+        for(a in 1..starNum) {
+            print("*")  
+        }
+        println("")
     }
 }
