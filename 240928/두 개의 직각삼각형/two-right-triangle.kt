8708fc76019f8f991a65a40e7c2817d2504400b5
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-28 22:58:01 +0900

    Title: 두 개의 직각삼각형, Time: 229ms, Memory: 12MB, Status: Passed - CodeTree

--- 240928/두 개의 직각삼각형/two-right-triangle.kt
+++ 240928/두 개의 직각삼각형/two-right-triangle.kt
@@ -6,19 +6,30 @@ fun main() {
     
     val n = sc.nextInt()
 
-    var notFirstLineEmptyCnt = 0
+    // 방법 1.
+    // var notFirstLineEmptyCnt = 0
 
+    // for(i in n downTo 1) {
+    //     if(n != i) {
+    //         val notFirstLineStarCnt = i
+    //         notFirstLineEmptyCnt += 2
+
+    //         print("${"*".repeat(notFirstLineStarCnt)}")
+    //         print("${" ".repeat(notFirstLineEmptyCnt)}")
+    //         println("${"*".repeat(notFirstLineStarCnt)}")
+    //     } else {
+    //         val firstLintCnt = n * 2
+    //         println("${"*".repeat(firstLintCnt)}")
+    //     }
+    // }
+
+    // 방법 2.(규칙 찾음)
     for(i in n downTo 1) {
-        if(n != i) {
-            val notFirstLineStarCnt = i
-            notFirstLineEmptyCnt += 2
+        val emptyCnt = 2 * (n - i)
 
-            print("${"*".repeat(notFirstLineStarCnt)}")
-            print("${" ".repeat(notFirstLineEmptyCnt)}")
-            println("${"*".repeat(notFirstLineStarCnt)}")
-        } else {
-            val firstLintCnt = n * 2
-            println("${"*".repeat(firstLintCnt)}")
-        }
+        print("${"*".repeat(i)}")
+        print("${" ".repeat(emptyCnt)}")
+        print("${"*".repeat(i)}")
+        println("")
     }
 }
