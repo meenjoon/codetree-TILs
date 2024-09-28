5feaff6b1799da3631f352db246720009b9dc8c5
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-28 16:50:48 +0900

    Title: 정사각형 두 개 출력, Time: 156ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240928/정사각형 두 개 출력/output-two-rectangles.kt
@@ -0,0 +1,17 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    val str = "*".repeat(n)
+
+    repeat(2) {
+        repeat(n) {
+            println(str)
+        }
+        println("")
+    }
+}
