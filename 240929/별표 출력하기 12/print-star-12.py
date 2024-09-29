e1d94ff4f94f8ba9922d2baff348c7492b1298dc
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-29 14:54:25 +0900

    Title: 별표 출력하기 12, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240929/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(n):
+    for j in range(n):
+        if i == 0:
+            print("*", end = " ")
+        else:
+            if j >= i and j % 2 == 1:
+                print("*", end = " ")
+            else:
+                print(" ", end = " ")
+    print()
