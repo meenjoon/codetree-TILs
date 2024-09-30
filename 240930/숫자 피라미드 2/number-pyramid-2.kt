1bc84f842d9b03de80222be6759f9dc429e83d10
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-30 22:40:48 +0900

    Title: 숫자 피라미드 2, Time: 197ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240930/숫자 피라미드 2/number-pyramid-2.kt
@@ -0,0 +1,17 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    
+    val n = sc.nextInt()
+
+    var cnt = 0
+    for(i in 1..n) {
+        for(j in 1..i) {
+            cnt += 1
+            print("$cnt ")
+        }
+        println("")
+    }
+}
