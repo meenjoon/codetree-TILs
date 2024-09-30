3991526d2d959b5601d933394e6aca9e5bdcef91
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-01 00:23:01 +0900

    Title: 정사각 알파벳 출력하기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241001/정사각 알파벳 출력하기/print-alpabet-in-square.kt
@@ -0,0 +1,18 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    var A = 65
+    for(i in 1..n) {
+        for(j in 1..n) {
+            print("${A.toChar()}")
+            A++
+        }
+        println("")
+    }
+    
+}
