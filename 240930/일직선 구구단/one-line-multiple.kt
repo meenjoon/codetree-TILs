158b769dee27a9c98bd62d8315b65cdb5f2f676d
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-09-30 23:39:30 +0900

    Title: 일직선 구구단, Time: 152ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240930/일직선 구구단/one-line-multiple.kt
@@ -0,0 +1,14 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    
+    val n = sc.nextInt()
+
+    for(i in 1..n) {
+        for(j in 1..n) {
+            println("$i * $j = ${i * j}")
+        }
+    }
+}
