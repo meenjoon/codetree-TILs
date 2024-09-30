9d747194ac552019d39ec76c2d18abe803ec2805
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-01 00:32:36 +0900

    Title: 알파벳 출력하기, Time: 128ms, Memory: 11MB, Status: Passed - CodeTree

--- 241001/알파벳 출력하기/print-alphabet.kt
+++ 241001/알파벳 출력하기/print-alphabet.kt
@@ -9,6 +9,11 @@ fun main() {
     var A = 65
     for(i in 1..n) {
         for(j in 1..i) {
+            val Z = 65 + 25
+            if(A > Z) {
+                A = 65
+            }
+            
             print("${A.toChar()}")
             A++
         }
