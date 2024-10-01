8eb6afb1881ded98d6dddd7e25ac4e73edd29b8a
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-01 16:14:36 +0900

    Title: 원소 10개의 합, Time: 167ms, Memory: 15MB, Status: Passed - CodeTree

--- 241001/원소 10개의 합/sum-of-10-elements.kt
+++ 241001/원소 10개의 합/sum-of-10-elements.kt
@@ -4,10 +4,22 @@ import kotlin.math.*
 fun main() {
     val sc = Scanner(System.`in`)
 
+    // var sumNum = 0
+    // repeat(10) {
+    //     val a = sc.nextInt()
+    //     sumNum += a
+    // }
+    // print("$sumNum")
+
+    val numbers = IntArray(10)
     var sumNum = 0
-    repeat(10) {
+    
+    for(i in numbers.indices) {
         val a = sc.nextInt()
-        sumNum += a
+        numbers[i] = a
     }
+
+    sumNum = numbers.sum()
+
     print("$sumNum")
 }
