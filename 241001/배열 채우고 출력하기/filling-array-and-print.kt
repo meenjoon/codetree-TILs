cde0eb7099dfb5d75403967de1710828b4fa4745
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-01 17:03:15 +0900

    Title: 배열 채우고 출력하기, Time: 201ms, Memory: 17MB, Status: Passed - CodeTree

--- 241001/배열 채우고 출력하기/filling-array-and-print.kt
+++ 241001/배열 채우고 출력하기/filling-array-and-print.kt
@@ -4,17 +4,22 @@ import kotlin.math.*
 fun main() {
     val sc = Scanner(System.`in`)
 
-    // 10개의 문자를 저장할 수 있는 배열 선언
-    val charArray = CharArray(10)
+    // // 10개의 문자를 저장할 수 있는 배열 선언
+    // val charArray = CharArray(10)
 
-    // 배열에 문자 입력 받기
-    for (i in charArray.indices) {
-        val a = sc.next()[0]
-        charArray[i] = a
-    }
+    // // 배열에 문자 입력 받기
+    // for (i in charArray.indices) {
+    //     val a = sc.next()[0]
+    //     charArray[i] = a
+    // }
 
-    // 배열을 거꾸로 출력
-    val reversedArray = charArray.reversedArray()
+    // // 배열을 거꾸로 출력
+    // val reversedArray = charArray.reversedArray()
 
-    println(reversedArray.joinToString(""))
+    // println(reversedArray.joinToString(""))
+
+    // val a = sc.nextLine().trim().split(" ").joinToString(separator = "").reversed()
+    // val a = sc.nextLine().trim().replace(" ", "").reversed()
+    val a = sc.nextLine().trim().split(" ").reversed().joinToString(separator = "")
+    println("$a")
 }
