7caf937c584d8e662bb0cb4df36cf322e7097a6f
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-02 23:03:19 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 165ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 241002/시험 통과 여부 확인하기 2/verify-test-passed-2.kt
@@ -0,0 +1,23 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    sc.nextLine()
+
+    var cnt = 0
+    repeat(3) {
+        val a = sc.nextLine().trim().split(" ").map{ it.toInt() }.sum() / 4
+        if(a >= 60){
+            println("pass")
+            cnt++ 
+        } else {
+            println("fail")
+        } 
+    }
+
+    print("$cnt")
+}
