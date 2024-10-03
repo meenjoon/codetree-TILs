d131dfbc68c4eab7a1e4561d873e8e61f3da0c21
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-03 17:29:40 +0900

    Title: 십의 자리 숫자, Time: 169ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 241003/십의 자리 숫자/number-of-tens-digit.kt
+++ 241003/십의 자리 숫자/number-of-tens-digit.kt
@@ -6,16 +6,27 @@ fun main() {
 
     val a = sc.nextLine().trim().split(" ").map{ it.toInt() }
 
-    val array = IntArray(9)
+    val array = IntArray(100)
+    val array2= IntArray(9)
 
     a.forEachIndexed { index, value ->
         val result = value / 10
         if(value >= 10) {
-             array[result - 1] += 1
+            // println("$value / $result")
+            if(array[result] != value) {
+                array[value] += 1
+            }
         }    
     }
 
     array.forEachIndexed { index, value ->
+        if(value > 0) {
+            val result = index / 10
+            array2[result-1] += 1
+        }
+    }
+    
+    array2.forEachIndexed { index, value ->
         println("${index + 1} - $value")
     }
 }
