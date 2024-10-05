75425e3eb55bf2fe1a94d2afce334eeee2a33dfe
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-06 00:53:42 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241006/배열 놀이/play-with-array.kt
@@ -0,0 +1,44 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val (n, q) = sc.nextLine().trim().split(" ").map{ it.toInt() }
+
+    val a = sc.nextLine().trim().split(" ").map{ it.toInt() }
+
+    var type = 1
+    repeat(q) {
+        if(type > 3) {
+            type = 1
+        }
+
+        val b = sc.nextLine().trim().split(" ").map{ it.toInt() }
+
+        if(type == 1) {
+            val num = b[b.lastIndex]
+            println("${a[num-1]}")
+        } else if(type == 2) {
+            val num = b[b.lastIndex]
+            val findAOfBIndex = a.indexOf(num)
+            println("${if(findAOfBIndex == -1) "0" else findAOfBIndex + 1}")
+        } else {
+            var startNum = 0
+            var endNum = 0
+            b.forEachIndexed { index, value ->
+                if(index == 1) {
+                    startNum = value
+                } else if(index == 2) {
+                    endNum = value
+                }
+            }
+            for(i in startNum-1..endNum-1) {
+                print("${a[i]} ")
+            }
+            println("")
+        }
+
+        type++
+    }
+}
