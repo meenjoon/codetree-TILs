0ab39423881a71809e2aa45eb097aa1b40d0dcad
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-06 15:07:27 +0900

    Title: 연속부분수열일까, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

--- 241006/연속부분수열일까/contiguous-array-or-not.kt
+++ 241006/연속부분수열일까/contiguous-array-or-not.kt
@@ -10,56 +10,84 @@ fun main() {
     
     val b = sc.nextLine().trim().split(" ").map { it.toInt() }
 
-    val cntSet = mutableSetOf<Int>()
-
-    var flag = false
-    var satisfiedIndex = -1
-    var cnt = 0
-    
+    var isSatisfied = false
+   
     for(i in 0..a.lastIndex) {
+         var success = true
+
         for(j in 0..b.lastIndex) {
-            if(flag) {
-                if(satisfiedIndex + 1 <= b.lastIndex) {
-                    if(a[i] == b[satisfiedIndex + 1]) {
-                        flag = true
-                        cnt++
-                        satisfiedIndex += 1
-                        cntSet.add(cnt)
-                        break
-                    } else {
-                        if(a[i] == b[j]) {
-                            cnt = 0
-                            flag = true
-                            satisfiedIndex = j
-                            cnt++
-                            cntSet.add(cnt)
-                            break
-                        } else {
-                            flag = false
-                        }
-                    }
-                }
+            if(i + j >= a.size) {
+                success = false
+                break
             }
 
-            if(a[i] == b[j]) {
-                flag = true
-                satisfiedIndex = j
-                cnt++
-                cntSet.add(cnt)
+            if(a[i + j] != b[j]) {
+                success = false
                 break
-            } else {
-                flag = false
-            }           
+            }
         }
 
-        if(flag == false) {
-            cnt = 0
+        if(success) {
+            println("Yes")
+            isSatisfied = true
+            break
         }
     }
-
-    if(cntSet.maxOrNull() == b.size) {
-        print("Yes")
-    } else {
-        print("No")
+    if(!isSatisfied) {
+        println("No")
     }
+
+    
+    // val cntSet = mutableSetOf<Int>()
+
+    // var flag = false
+    // var satisfiedIndex = -1
+    // var cnt = 0
+    
+    // for(i in 0..a.lastIndex) {
+    //     for(j in 0..b.lastIndex) {
+    //         if(flag) {
+    //             if(satisfiedIndex + 1 <= b.lastIndex) {
+    //                 if(a[i] == b[satisfiedIndex + 1]) {
+    //                     flag = true
+    //                     cnt++
+    //                     satisfiedIndex += 1
+    //                     cntSet.add(cnt)
+    //                     break
+    //                 } else {
+    //                     if(a[i] == b[j]) {
+    //                         cnt = 0
+    //                         flag = true
+    //                         satisfiedIndex = j
+    //                         cnt++
+    //                         cntSet.add(cnt)
+    //                         break
+    //                     } else {
+    //                         flag = false
+    //                     }
+    //                 }
+    //             }
+    //         }
+
+    //         if(a[i] == b[j]) {
+    //             flag = true
+    //             satisfiedIndex = j
+    //             cnt++
+    //             cntSet.add(cnt)
+    //             break
+    //         } else {
+    //             flag = false
+    //         }           
+    //     }
+
+    //     if(flag == false) {
+    //         cnt = 0
+    //     }
+    // }
+
+    // if(cntSet.maxOrNull() == b.size) {
+    //     print("Yes")
+    // } else {
+    //     print("No")
+    // }
 }
