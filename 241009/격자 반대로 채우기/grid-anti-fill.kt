6df6f86982f5b2c65e172486c02385dfe2c3a1e6
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-09 23:18:56 +0900

    Title: 격자 반대로 채우기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 241009/격자 반대로 채우기/grid-anti-fill.kt
+++ 241009/격자 반대로 채우기/grid-anti-fill.kt
@@ -9,26 +9,55 @@ fun main() {
     val array = Array(n) { IntArray(n) }
 
     var cnt = 1
-    for(col in n-1 downTo 0) {
-        val isEvenRow = if(col % 2 == 0) true else false
 
-        var currRow = if(isEvenRow) {
-            0
-        } else {
-            n-1
-        }
-        var currColumn = col
-        
-        while(currRow >= 0 && currRow <= n-1 && currColumn >= 0 && currColumn <= n-1) {
-            array[currRow][currColumn] = cnt
+    val isEvenN = if(n % 2 == 0) true else false
 
-            if(isEvenRow) {
-                currRow++
+    
+    if(isEvenN) {
+        for(col in n-1 downTo 0) {
+            val isEvenRow = if(col % 2 == 0) true else false
+
+            var currRow = if(isEvenRow) {
+                0
             } else {
-                currRow--
+                n-1
             }
+            var currColumn = col
             
-            cnt++
+            while(currRow >= 0 && currRow <= n-1 && currColumn >= 0 && currColumn <= n-1) {
+                array[currRow][currColumn] = cnt
+
+                if(isEvenRow) {
+                    currRow++
+                } else {
+                    currRow--
+                }
+                
+                cnt++
+            }
+        }
+    } else {
+        for(col in n-1 downTo 0) {
+            val isEvenRow = if(col % 2 == 0) true else false
+
+            var currRow = if(isEvenRow) {
+                n-1
+            } else {
+                0
+            }
+            var currColumn = col
+            
+            while(currRow >= 0 && currRow <= n-1 && currColumn >= 0 && currColumn <= n-1) {
+                array[currRow][currColumn] = cnt
+
+                if(isEvenRow) {
+                    currRow--
+                } else {
+                    currRow++
+                }
+                
+                cnt++
+            }
         }
     }
 
