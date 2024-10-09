51b237cbc0537ea32368a4e1efb0624eefd1a740
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-09 20:29:28 +0900

    Title: 지그재그로 숫자 채우기, Time: 215ms, Memory: 14MB, Status: Passed - CodeTree

--- 241009/지그재그로 숫자 채우기/zigzag-numbering.kt
+++ 241009/지그재그로 숫자 채우기/zigzag-numbering.kt
@@ -9,15 +9,32 @@ fun main() {
     
     val array = Array(n) { IntArray(m) }
 
-    for(i in 0..n-1) {
-        for(j in 0..m-1) {
-            if(j % 2 == 0) {
-                array[i][j] = (j * n) + i
-            } else {
-                array[i][j] = ((j + 1) * n) - (i + 1)
+    // 방법 1.
+    var cnt = 0
+    for(i in 0..m-1) {
+        if(i % 2 == 0) {
+            for(j in 0..n-1) {
+                array[j][i] = cnt
+                cnt++
+            }
+        } else {
+            for(j in n-1 downTo 0) {
+                array[j][i] = cnt
+                cnt++
             }
         }
     }
+
+    // 방법 2.
+    // for(i in 0..n-1) {
+    //     for(j in 0..m-1) {
+    //         if(j % 2 == 0) {
+    //             array[i][j] = (j * n) + i
+    //         } else {
+    //             array[i][j] = ((j + 1) * n) - (i + 1)
+    //         }
+    //     }
+    // }
 
     for(i in array.indices) {
         for(j in array[i].indices) {
