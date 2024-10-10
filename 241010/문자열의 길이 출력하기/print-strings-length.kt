02c424e615b80b4a9eaf2aa67df68f4a0b828d13
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-10 19:55:48 +0900

    Title: 문자열의 길이 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241010/문자열의 길이 출력하기/print-strings-length.kt
@@ -0,0 +1,13 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.next()
+    sc.nextLine()
+    val b = sc.next()
+
+    var length = a.length + b.length
+    println("${length}")
+}
