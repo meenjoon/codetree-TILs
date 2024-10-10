11500e0be68d1cac1c80183cffd3e7fa1a74b1cc
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-10 20:25:17 +0900

    Title: 공백을 기준으로 출력, Time: 163ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241010/공백을 기준으로 출력/output-based-on-space.kt
@@ -0,0 +1,13 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.nextLine().trim()
+
+    val b = sc.nextLine().trim()
+
+    val ab = (a + b).replace(" ", "")
+    print("$ab")
+}
