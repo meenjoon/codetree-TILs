a9ad9d3adf6ac96d4fc0a5124e2e80789f1207a6
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-10 20:54:49 +0900

    Title: 단어로 구분하기, Time: 164ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241010/단어로 구분하기/separate-words-with-words.kt
@@ -0,0 +1,12 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.nextLine().trim().split(" ")
+
+    a.forEach {
+        println("$it")
+    }
+}
