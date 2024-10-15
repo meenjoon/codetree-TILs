d01d799e8de50ed0962a95a6babc7c44af6e8db4
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-15 19:29:46 +0900

    Title: 문자 제거하기, Time: 145ms, Memory: 11MB, Status: Passed - CodeTree

--- 241015/문자 제거하기/del-char.kt
+++ 241015/문자 제거하기/del-char.kt
@@ -4,20 +4,37 @@ import kotlin.math.*
 fun main() {
     val sc = Scanner(System.`in`)
     
-    var str = sc.nextLine()
+    // var str = sc.nextLine()
 
+    // while(true) {
+    //     val n = sc.nextLine().toInt()
+
+    //     if(str.length <= n) {
+    //         str = str.slice(0..str.length-2)
+    //     } else {
+    //         str = str.slice(0..n-1) + str.slice(n+1..str.length-1)
+    //     }
+
+    //     println("$str")
+
+    //     if(str.length == 1) {
+    //         break
+    //     }
+    // }
+
+    val sb = StringBuilder(sc.nextLine())
+    
     while(true) {
         val n = sc.nextLine().toInt()
 
-        if(str.length <= n) {
-            str = str.slice(0..str.length-2)
+        if(sb.length <= n) {
+            sb.delete(sb.length-1, sb.length)
         } else {
-            str = str.slice(0..n-1) + str.slice(n+1..str.length-1)
+            sb.delete(n, n+1)
         }
+        println("${sb.toString()}")
 
-        println("$str")
-
-        if(str.length == 1) {
+        if(sb.length == 1) {
             break
         }
     }
