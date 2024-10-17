8acb6e36e93ad6b6a6a0bc156e10f730f8c923e1
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-17 22:48:37 +0900

    Title: 아스키코드 변환, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241017/아스키코드 변환/convert-to-ascii.kt
@@ -0,0 +1,11 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val a = sc.next()
+    val n = sc.nextInt()
+
+    print("${a[0].code} ${n.toChar()}")
+}
