d875f429bd515b0d1871bab75f264f5215d3f83f
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-22 19:55:42 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 123ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241022/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.kt
@@ -0,0 +1,14 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    
+    printStar(cnt = 5)
+}
+
+fun printStar(cnt: Int) {
+    repeat(cnt) {
+        println("**********")
+    }
+}
