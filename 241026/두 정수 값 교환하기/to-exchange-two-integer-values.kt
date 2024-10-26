c2a392c60ecdcc1d44932d8ea32728a7685f0ca9
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-26 20:46:23 +0900

    Title: 두 정수 값 교환하기, Time: 172ms, Memory: 14MB, Status: Passed - CodeTree

--- 241026/두 정수 값 교환하기/to-exchange-two-integer-values.kt
+++ 241026/두 정수 값 교환하기/to-exchange-two-integer-values.kt
@@ -1,23 +1,38 @@
 import java.util.*
 import kotlin.math.*
 
+class DummyNum(var num: Int)
+
 fun main() {
     val sc = Scanner(System.`in`)
 
     val (a, b) = sc.nextLine().trim().split(" ").map { it.toInt() }
+    val dummyNum1 = DummyNum(num = a)
+    val dummyNum2 = DummyNum(num = b) 
 
-    val(newA, newB) = swap(a = a, b = b)
-    print("$newA $newB")
+    swap(a = dummyNum1, b = dummyNum2)
+
+    print("${dummyNum1.num} ${dummyNum2.num}")
 }
 
-fun swap(
-    a: Int,
-    b: Int,
-): Pair<Int, Int> {
-    val temp = a
+// fun swap(
+//     a: Int,
+//     b: Int,
+// ): Pair<Int, Int> {
+//     val temp = a
     
-    val newA = b
-    val newB = temp
+//     val newA = b
+//     val newB = temp
 
-    return Pair(newA, newB)
+//     return Pair(newA, newB)
+// }
+
+fun swap(
+    a: DummyNum,
+    b: DummyNum,
+) {
+    val temp = a.num
+    
+    a.num = b.num
+    b.num = temp
 }
