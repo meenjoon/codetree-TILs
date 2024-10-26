3ee087976ebf5fc8753cfe13d404f32070c9e70e
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-26 22:54:34 +0900

    Title: 2개 이상의 알파벳, Time: 166ms, Memory: 11MB, Status: Passed - CodeTree

--- 241026/2개 이상의 알파벳/more-than-one-alphabet.kt
+++ 241026/2개 이상의 알파벳/more-than-one-alphabet.kt
@@ -1,30 +1,22 @@
 import java.util.*
 import kotlin.math.*
-
 fun main() {
     val sc = Scanner(System.`in`)
     
     val a = sc.next()
-
     val map = mutableMapOf<Char, Int>()
     differentString(str = a, map = map)
 
-    var isDifferent = false
+    var cnt = 0
     // map.entries.forEachIndexed { index, entry ->
-    //     if(entry.value >= 2) {
-    //         isDiffrent = true
-    //     }
+    //     cnt++
     // }
     for ((index, entry) in map.entries.withIndex()) {
-        if (entry.value >= 2) {
-            isDifferent = true
-            break
-        }
+        cnt++
     }
-
-    if(isDifferent) print("Yes") else print("No")
+    
+    if(cnt >= 2) print("Yes") else print("No")
 }
-
 fun differentString(
     str: String,
     map: MutableMap<Char, Int>,
