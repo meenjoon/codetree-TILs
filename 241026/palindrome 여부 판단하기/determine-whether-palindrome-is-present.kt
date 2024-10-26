425177430c61e564d0135554efa4585a38e606ba
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-26 21:57:54 +0900

    Title: palindrome 여부 판단하기, Time: 144ms, Memory: 11MB, Status: Passed - CodeTree

--- 241026/palindrome 여부 판단하기/determine-whether-palindrome-is-present.kt
+++ 241026/palindrome 여부 판단하기/determine-whether-palindrome-is-present.kt
@@ -7,11 +7,22 @@ fun main() {
     val sc = Scanner(System.`in`)
 
     var a = sc.next()
-    val b = reversed(a = a)
 
-    if (a == b) print("Yes") else print("No")
+    // val b = reversed(a = a)
+    // if (a == b) print("Yes") else print("No")
+
+    if(reversed(a = a)) print("Yes") else print("No")
 }
 
-fun reversed(a: String): String {
-    return a.reversed()
+// fun reversed(a: String): String {
+//     return a.reversed()
+// }
+
+
+fun reversed(a: String): Boolean {
+    for(i in 0..a.length / 2 - 1) {
+        if(a[i] != a[(a.length-1) - i]) return false
+    }
+
+    return true
 }
