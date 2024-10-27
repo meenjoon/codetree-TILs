8aa2a12984e1d667bb89b369eb9b8ca2ecbd446e
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-27 20:22:05 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 148ms, Memory: 11MB, Status: Passed - CodeTree

--- 241027/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.kt
+++ 241027/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.kt
@@ -15,12 +15,12 @@ fun isContain(
     b: String,
 ): Int {
     for(i in 0..a.length-1) {
-        val maxRange = a.length - 1
-        val wantRange = i + b.length
+        val aMaxRange = a.length - 1
+        val bMaxRange = b.length - 1
 
         var aIndex = i // a 문자열의 시작 인덱스, a 문자열은 한 개의 문자를 시작점으로 두고 아니면 그 다음 문자점으로 시작점을 둬야 하기에 초기값으로 i로 할당
         var bIndex = 0 // b 문자열의 시작 인덱스, b 문자열은 처음부터 끝까지 돌기에 초기값을 0으로 할당
-        while(aIndex <= maxRange) {
+        while(aIndex <= aMaxRange && bIndex <= bMaxRange) {
             if(a[aIndex] == b[bIndex]) {
                 aIndex++
                 bIndex++
