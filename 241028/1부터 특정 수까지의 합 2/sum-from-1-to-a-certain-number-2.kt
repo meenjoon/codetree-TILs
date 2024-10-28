ac293de49c7007a955dee0782c8529a485959a99
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-10-28 20:11:09 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 157ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241028/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.kt
@@ -0,0 +1,18 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    print(factorial(n = n))
+}
+
+fun factorial(n: Int): Int {
+    if(n == 1) {
+        return 1
+    }
+    
+    return factorial(n - 1) + n    
+}
