e21baea76a673f1ae2b43d707732d380c6b96004
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-11-13 22:31:15 +0900

    Title: 원점으로부터의 거리, Time: 273ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 241113/원점으로부터의 거리/distance-from-origin.kt
@@ -0,0 +1,37 @@
+import java.util.*
+import kotlin.math.*
+
+data class Dot(
+    val x: Int,
+    val y: Int,
+    val distance: Int = -1,
+    val index: Int,
+)
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextLine().toInt()
+
+    val list = List(n) {
+        Dot(
+            x = sc.nextInt(),
+            y = sc.nextInt(),
+            index = it + 1,
+        )
+    }
+
+    val formattedList = list.map {
+        it.copy(distance = abs(it.x) + abs(it.y))
+    }
+
+    formattedList.sortedWith(compareBy<Dot> { it.distance } 
+        .thenBy { it.index }
+    ).forEach {
+        println(it.index)
+    }
+
+    
+
+
+}
