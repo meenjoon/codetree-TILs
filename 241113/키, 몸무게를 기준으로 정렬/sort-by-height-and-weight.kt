4c380339bed45cde7743e5a20ff21678c786f8ca
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-11-13 22:04:43 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 204ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241113/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.kt
@@ -0,0 +1,32 @@
+import java.util.*
+import kotlin.math.*
+
+data class Person(
+    val name: String,
+    val height: Int,
+    val weight: Int,
+) {
+    override fun toString(): String {
+        return "$name $height $weight"
+    }
+}
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextLine().toInt()
+
+    val list = List(n) { 
+        Person(
+            name = sc.next(),
+            height = sc.nextInt(),
+            weight = sc.nextInt(),
+        )
+    }
+
+    list.sortedWith(compareBy<Person> { it.height }
+        .thenByDescending { it.weight }
+    ).forEach {
+        println(it)
+    }
+}
