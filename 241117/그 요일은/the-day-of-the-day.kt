6f7601204ba4793f059220cd047aae676833bd9a
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-11-17 18:05:32 +0900

    Title: 그 요일은, Time: 200ms, Memory: 19MB, Status: Passed - CodeTree

--- 241117/그 요일은/the-day-of-the-day.kt
+++ 241117/그 요일은/the-day-of-the-day.kt
@@ -7,16 +7,16 @@ fun main() {
     val (m1, d1, m2, d2) = sc.nextLine().trim().split(" ").map { it.toInt() }
     val day = sc.next()
 
-    val firstTotalDay = (1..(m1-1)).map { monthToDay(it) }.sum() + d1
-    val secondTotalDay = (1..(m2-1)).map { monthToDay(it) }.sum() + d2
+    val firstTotalDay = (1..(m1-1)).map { monthToDay(it) }.sum() + d1 // first의 총일 구하기
+    val secondTotalDay = (1..(m2-1)).map { monthToDay(it) }.sum() + d2 // second의 총일 구하기
 
-    val isPositive = secondTotalDay > firstTotalDay
+    val isPositive = secondTotalDay > firstTotalDay // second가 더 큰 지 확인 하는 변수
 
-    val diffDay = abs(firstTotalDay - secondTotalDay)
+    val diffDay = abs(firstTotalDay - secondTotalDay) // first와 second의 요일 차
 
-    val divideDay = diffDay / 7
-    val modDay = diffDay % 7
-    val includeDay = if(includeDay(modDay = modDay, compareDay = day, isPositive = isPositive)) 1 else 0
+    val divideDay = diffDay / 7 // first와 scond의 요일 차에서 7 나누기
+    val modDay = diffDay % 7 // first와 scond의 요일 차에서 7로 나눴을때 나머지
+    val includeDay = if(includeDay(modDayNum = modDay, compareDay = day, isPositive = isPositive)) 1 else 0
     
     if(isPositive) { // second 가 더 큰 경우
         println("${divideDay + includeDay}")
