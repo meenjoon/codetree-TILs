0c8db86eff5c90d189a464d5381959623aaa0b3f
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-11-18 20:41:11 +0900

    Title: 10진수로 변환하기, Time: 168ms, Memory: 12MB, Status: Passed - CodeTree

--- 241118/10진수로 변환하기/convert-to-decimal.kt
+++ 241118/10진수로 변환하기/convert-to-decimal.kt
@@ -7,7 +7,7 @@ fun main() {
     val a = sc.next().reversed()
 
     var num = 0
-    for(i in 0..a.length) {
+    for(i in 0..a.length-1) {
         num += (a[i] - '0') * (2.0).pow(i).toInt()
     }
 
