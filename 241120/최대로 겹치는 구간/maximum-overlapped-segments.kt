af74158e0b9798b90465f0fe2d45eee0dfa62de8
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-11-20 22:45:39 +0900

    Title: 최대로 겹치는 구간, Time: 188ms, Memory: 13MB, Status: Passed - CodeTree

--- 241120/최대로 겹치는 구간/maximum-overlapped-segments.kt
+++ 241120/최대로 겹치는 구간/maximum-overlapped-segments.kt
@@ -10,26 +10,28 @@ fun main() {
     repeat(n) {
         val (a, b) = sc.nextLine().trim().split(" ").map { it.toInt() }
 
-        for(i in a..b) {
+        for(i in a..b-1) {
             map[i] = map.getOrDefault(i, 0) + 1
         }
     }
 
-    val formattedMap = map.values.filter { it >= n }
+    // val formattedMap = map.values.filter { it >= n }
 
-    var rememberValue = Int.MIN_VALUE
-    var duplicatedMaxNum = 0
-    for((index, entry) in map.entries.withIndex()) {
-        if(index == 0) {
-            rememberValue = entry.value
-            continue
-        } else {
-            if((rememberValue == entry.value) && (duplicatedMaxNum <= rememberValue)) {
-                duplicatedMaxNum = rememberValue
-            }
-            rememberValue = entry.value
-        }
-    }
+    // var rememberValue = Int.MIN_VALUE
+    // var duplicatedMaxNum = 0
+    // for((index, entry) in map.entries.withIndex()) {
+    //     if(index == 0) {
+    //         rememberValue = entry.value
+    //         continue
+    //     } else {
+    //         if((rememberValue == entry.value) && (duplicatedMaxNum <= rememberValue)) {
+    //             duplicatedMaxNum = rememberValue
+    //         }
+    //             rememberValue = entry.value
+    //         }
+    //     }
 
-    print(duplicatedMaxNum)
+    //     print(duplicatedMaxNum)
+
+    print(map.values.maxOrNull())
 }
