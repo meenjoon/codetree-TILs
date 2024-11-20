67a35ede55dc187a8e8240876a5b5282274a61fa
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-11-20 22:34:33 +0900

    Title: 최대로 겹치는 구간, Time: 178ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 241120/최대로 겹치는 구간/maximum-overlapped-segments.kt
@@ -0,0 +1,35 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextLine().toInt()
+
+    val map = mutableMapOf<Int, Int>()
+    repeat(n) {
+        val (a, b) = sc.nextLine().trim().split(" ").map { it.toInt() }
+
+        for(i in a..b) {
+            map[i] = map.getOrDefault(i, 0) + 1
+        }
+    }
+
+    val formattedMap = map.values.filter { it >= n }
+
+    var rememberValue = Int.MIN_VALUE
+    var duplicatedMaxNum = 0
+    for((index, entry) in map.entries.withIndex()) {
+        if(index == 0) {
+            rememberValue = entry.value
+            continue
+        } else {
+            if((rememberValue == entry.value) && (duplicatedMaxNum <= rememberValue)) {
+                duplicatedMaxNum = rememberValue
+            }
+            rememberValue = entry.value
+        }
+    }
+
+    print(duplicatedMaxNum)
+}
