759cd79f7b611f7c5359ea3dd5a916a714153549
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-12-06 23:50:41 +0900

    Title: 벌금은 누구에게, Time: 183ms, Memory: 13MB, Status: Runtime Error - CodeTree

--- 241206/벌금은 누구에게/who-will-pay.kt
+++ 241206/벌금은 누구에게/who-will-pay.kt
@@ -6,18 +6,37 @@ fun main() {
 
     val (n, m, k) = sc.nextLine().trim().split(" ").map { it.toInt() }
     
-    val map = mutableMapOf<Int, Int>()
+    val personArray = IntArray(100)
+    val wroingArray = IntArray(100) 
+
+    for(i in 0..m-1) {
+        personArray[i] = sc.nextInt()
+    }
 
     var resultNum = -1
-    for(i in 0..m-1) {
-        val a = sc.nextInt()
 
-        map[a] = map.getOrDefault(a, 0) + 1
-        if(map[a] == k) {
-            resultNum = a
+    for(i in 0..m-1) {
+        val num = personArray[i]
+        wroingArray[num] += 1
+
+        if(wroingArray[num] == k) {
+            resultNum = num
             break
         }
     }
+
+    // val map = mutableMapOf<Int, Int>()
+
+    // var resultNum = -1
+    // for(i in 0..m-1) {
+        // val a = sc.nextInt()
+        
+        // map[a] = map.getOrDefault(a, 0) + 1
+        // if(map[a] == k) {
+        //     resultNum = a
+        //     break
+        // }
+    // }
 
     print(resultNum)
 }
