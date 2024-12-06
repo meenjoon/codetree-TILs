a515b05c23217c5757c500474dcdf79b522e21b8
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-12-07 00:07:41 +0900

    Title: 벌금은 누구에게, Time: 230ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241207/벌금은 누구에게/who-will-pay.kt
@@ -0,0 +1,42 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val (n, m, k) = sc.nextLine().trim().split(" ").map { it.toInt() }
+    
+    val personArray = IntArray(10000)
+    val wrongArray = IntArray(101) 
+
+    for(i in 0..m-1) {
+        personArray[i] = sc.nextInt()
+    }
+
+    var resultNum = -1
+
+    for(i in 0..m-1) {
+        val num = personArray[i]
+        wrongArray[num] += 1
+
+        if(wrongArray[num] == k) {
+            resultNum = num
+            break
+        }
+    }
+
+    // val map = mutableMapOf<Int, Int>()
+
+    // var resultNum = -1
+    // for(i in 0..m-1) {
+        // val a = sc.nextInt()
+        
+        // map[a] = map.getOrDefault(a, 0) + 1
+        // if(map[a] == k) {
+        //     resultNum = a
+        //     break
+        // }
+    // }
+
+    print(resultNum)
+}
