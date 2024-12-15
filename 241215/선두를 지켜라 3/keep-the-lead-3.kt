094a11d6d5c8da63ddb3fb3f73ea8ae337847840
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-12-15 17:56:08 +0900

    Title: 선두를 지켜라 3, Time: 289ms, Memory: 26MB, Status: Passed - CodeTree

--- 241215/선두를 지켜라 3/keep-the-lead-3.kt
+++ 241215/선두를 지켜라 3/keep-the-lead-3.kt
@@ -20,13 +20,16 @@ fun main() {
 
         val (v, t) = sc.nextLine().trim().split(" ").map { it.toInt() }
 
+        // arrayA에 특정 속도로 특정 시간만큼 이동 한 것 할당
         if(parenIndex < n) {
             repeat(t) {
                 distanceA += v
                 arrayA[timeA + it] = distanceA
             }
             timeA += t
-        } else {
+        } 
+        // arrayB에 특정 속도로 특정 시간만큼 이동 한 것 할당   
+        else {
             repeat(t) {
                 distanceB += v
                 arrayB[timeB + it] = distanceB
@@ -35,24 +38,27 @@ fun main() {
         }
     }
 
-    var chageFlag = if(arrayA[0] == arrayB[0]) Status.DRAW else if(arrayA[0] > arrayB[0]) Status.WINA else Status.WINB
+    var rankingStatus = if(arrayA[0] == arrayB[0]) Status.DRAW else if(arrayA[0] > arrayB[0]) Status.WINA else Status.WINB
     var resultCnt = 0
     for(i in 0..arrayA.size-1) {
         val numA = arrayA[i]
         val numB = arrayB[i]
 
+        // 아무 값이 들어있지 않다면(Int.MIN_VALUE) break
         if(numA == Int.MIN_VALUE && numB == Int.MIN_VALUE) {
             break
         }
 
+        // index가 0일 경우 continue
         if(i == 0) {
             resultCnt++
             continue
         }
 
-        val tempChangeFlag = chageFlag
-        chageFlag = if(numA == numB) Status.DRAW else if(numA > numB) Status.WINA else Status.WINB
-        if(tempChangeFlag != chageFlag) {
+        // 이전 인덱스의 rankingStatus 상태와 현재 인덱스의 rankingStatus 상태가 다를 경우에 resultCnt++
+        val tempRankingStatus = rankingStatus
+        rankingStatus = if(numA == numB) Status.DRAW else if(numA > numB) Status.WINA else Status.WINB
+        if(tempRankingStatus != rankingStatus) {
             resultCnt++
         }
     }
@@ -60,6 +66,7 @@ fun main() {
     print(resultCnt)
 }
 
+// 명예의 전당 상태를 위한 enum class
 enum class Status() {
     WINA,
     WINB,
