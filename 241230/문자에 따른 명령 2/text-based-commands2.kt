7976df64b5a7d25cf52afa9bc825aaa9b509fc97
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-12-30 21:11:11 +0900

    Title: 문자에 따른 명령 2, Time: 200ms, Memory: 11MB, Status: Runtime Error - CodeTree

---
+++ 241230/문자에 따른 명령 2/text-based-commands2.kt
@@ -0,0 +1,33 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val dirString = sc.nextLine()
+
+    val arrayX = intArrayOf(1, 0, -1, 0)
+    val arrayY = intArrayOf(0, -1, 0, 1)
+
+    var currDirection = 3 // 북쪽
+
+    var x = 0
+    var y = 0
+
+    for(i in 0..dirString.length-1) {
+        val dir = dirString[i]
+        
+        if(dir == 'L') {
+           val resultDirection = currDirection - 1
+           currDirection = if(resultDirection == 0) 3 else resultDirection
+        } else if(dir == 'R') {
+            val resultDirection = currDirection + 1
+            currDirection = if(resultDirection == 3) 0 else resultDirection
+        } else {
+            x += arrayX[currDirection]
+            y += arrayY[currDirection]
+        }
+    }
+
+    print("$x $y")
+}
