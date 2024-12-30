f53c70317ee13539f737a66e4d47ea8b81a709b0
meenjoon <88024665+meenjoon@users.noreply.github.com>
2024-12-30 20:35:24 +0900

    Title: 방향에 맞춰 이동, Time: 168ms, Memory: 12MB, Status: Passed - CodeTree

--- 241230/방향에 맞춰 이동/move-in-direction.kt
+++ 241230/방향에 맞춰 이동/move-in-direction.kt
@@ -6,17 +6,8 @@ fun main() {
 
     val n = sc.nextLine().toInt()
 
-    val arrayX = IntArray(4)
-    // arrayX[0] = 1
-    // arrayX[1] = 0
-    // arrayX[2] = -1
-    // arrayX[3] = 0
-
-    val arrayY = IntArray(4)
-    // arrayY[0] = 0
-    // arrayY[1] = -1
-    // arrayY[2] = 0
-    // arrayY[3] = 1
+    val arrayX = intArrayOf(1, 0, -1, 0)
+    val arrayY = intArrayOf(0, -1, 0, 1)
 
     var resultX = 0
     var resultY = 0
@@ -35,8 +26,6 @@ fun main() {
             resultY = resultY
         )
 
-        // println("$calX $calY")
-
         resultX = calX
         resultY = calY
     }
@@ -52,25 +41,23 @@ fun calDistance(
     resultX: Int,
     resultY: Int,
     ): Pair<Int, Int> {
-    var x = 0
-    var y = 0
+    var x = resultX
+    var y = resultY
+
+    var dir = 0
 
     if(allow == "E") {
-        x = arrayX[0] + distance + resultX
-        y = resultY
+        dir = 0
     } else if(allow == "S") {
-        x = resultX
-        y = arrayY[1] - distance + resultY
+        dir = 1
     } else if(allow == "W") {
-        x = arrayX[2] - distance + resultX
-        y = resultY
+        dir = 2
     } else {
-        x = resultX
-        y = arrayY[3] + distance + resultY
+        dir = 3
     }
+    
+    x += arrayX[dir] * distance
+    y += arrayY[dir] * distance
 
     return Pair(first = x, second = y)
 }
-
-// 1 0 -1 0
-// 0 -1 0 1
