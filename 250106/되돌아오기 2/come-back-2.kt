0dad80569ba518b999826ec7ebf4be1b45a5001a
meenjoon <88024665+meenjoon@users.noreply.github.com>
2025-01-06 22:26:45 +0900

    Title: 되돌아오기 2, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250106/되돌아오기 2/come-back-2.kt
@@ -0,0 +1,56 @@
+import java.util.*
+import kotlin.math.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    
+    val direction = sc.nextLine()
+
+    val dx = intArrayOf(1, 0 , -1, 0)
+    val dy = intArrayOf(0, -1, 0, 1)
+    // 0 위
+    // 3 오른
+    // 2 아래
+    // 1 왼
+
+    var x = 0
+    var y = 0
+
+    var t = 0
+    var currTime = -1
+    var isSatisfied = false
+
+    var currDirection = 0
+
+    for(i in 0..direction.length-1) {
+        if(isSatisfied) {
+            break
+        }
+
+        if(direction[i] == 'F') {
+            t += 1
+            x += dx[currDirection]
+            y += dy[currDirection]
+
+            if(x == 0 && y == 0) {
+                currTime = t
+                isSatisfied = true
+                break
+            }
+        } else {
+            t += 1
+            x += dx[currDirection]
+            y += dy[currDirection]
+            
+            currDirection = if(direction[i] == 'R') (currDirection - 1 + 4) % 4 else (currDirection + 1) % 4
+                
+            if(x == 0 && y == 0) {
+                currTime = t
+                isSatisfied
+                break
+            }
+        }
+    }
+
+    print("${currTime-1}")
+}
